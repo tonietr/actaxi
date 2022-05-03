68247be3fac4b4a66ecd009b5e402519ee5b1998
tonietr <92841796+tonietr@users.noreply.github.com>
2022-05-03 11:05:57 -0700

    Update

--- config/db.config.js
+++ config/db.config.js
@@ -1,30 +1,30 @@
 //Devs
-module.exports = {
-    HOST: "localhost",
-    USER: "actaxi_admin",
-    PASSWORD: "NirvanaCanada4745891!",
-    DB: "actaxi_TripData",
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-}
-
-//Production
 // module.exports = {
-//     HOST: "db-mysql-tor1-45825-do-user-11433732-0.b.db.ondigitalocean.com",
-//     USER: "doadmin",
-//     PASSWORD: "AVNS_FbffI5_MfdL60aC",
+//     HOST: "localhost",
+//     USER: "actaxi_admin",
+//     PASSWORD: "NirvanaCanada4745891!",
 //     DB: "actaxi_TripData",
 //     dialect: "mysql",
-//     port: 25060,
 //     pool: {
 //         max: 5,
 //         min: 0,
 //         acquire: 30000,
-//         idle: 30000
+//         idle: 10000
 //     }
 // }
+
+//Production
+module.exports = {
+    HOST: "db-mysql-tor1-45825-do-user-11433732-0.b.db.ondigitalocean.com",
+    USER: "doadmin",
+    PASSWORD: "AVNS_FbffI5_MfdL60aC",
+    DB: "actaxi_TripData",
+    dialect: "mysql",
+    port: 25060,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 30000
+    }
+}
